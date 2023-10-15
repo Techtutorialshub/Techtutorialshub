a019f6a080815b0673a1829d7948cee457fab9f4
Tech Tutorials hub <125792165+Techtutorialshub@users.noreply.github.com>
2023-10-15 20:55:06 +0000

    Create json-ld-script1.js
    Schema for website

---
+++ json-ld-script1.js
@@ -0,0 +1,11 @@
+{
+  "@context": "https://schema.org/",
+  "@type": "WebSite",
+  "name": "Tech Tutorials Hub",
+  "url": "https://www.techtutorialshub.com",
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://www.techtutorialshub.com/search{search_term_string}?m=1",
+    "query-input": "required name=search_term_string"
+  }
+}
