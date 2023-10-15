7dcf6a3f9a4f81650cd7d93b66f1f991dcba104e
Tech Tutorials hub <125792165+Techtutorialshub@users.noreply.github.com>
2023-10-15 20:53:30 +0000

    Create json-ld-script2.js

---
+++ json-ld-script2.js
@@ -0,0 +1,30 @@
+{
+  "@context": "https://schema.org/",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+    "@type": "ListItem",
+    "position": 1,
+    "name": "Home",
+    "item": "https://www.techtutorialshub.com"
+  },{
+    "@type": "ListItem",
+    "position": 2,
+    "name": "About Us",
+    "item": "https://www.techtutorialshub.com/p/about-us.html"
+  },{
+    "@type": "ListItem",
+    "position": 3,
+    "name": "Tutorials",
+    "item": "https://www.techtutorialshub.com/search/label/How%20to"
+  },{
+    "@type": "ListItem",
+    "position": 4,
+    "name": "Android",
+    "item": "https://www.techtutorialshub.com/search/label/Android"
+  },{
+    "@type": "ListItem",
+    "position": 5,
+    "name": "Disclaimer",
+    "item": "https://www.techtutorialshub.com/p/disclaimer.html"
+  }]
+}
